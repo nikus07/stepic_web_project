b652ac7afcf104d8593761759ddf5fd6a75f35df
Muntello <muntello@gmail.com>
2016-03-09 22:33:03 +0300

    hello

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
-def application(environ, start_response):
+def wsgi_application(environ, start_response):
 	status = "200 OK"
  	headers = [("Content-Type", "text/plain")]
  	start_response(status, headers)
  	body = "\n".join(environ["QUERY_STRING"].split("&"))
- 	return body
+ 	return [ body ]
  if __name__ == '__main__':
  	pass
