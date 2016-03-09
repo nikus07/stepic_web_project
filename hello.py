3f054bf8ee9adb1460d5716a979095fc67bddcd6
Muntello <muntello@gmail.com>
2016-03-09 22:51:38 +0300

    hello.py changed

--- hello.py
+++ hello.py
@@ -1,8 +1,7 @@
-def wsgi_application(environ, start_response):
-	status = "200 OK"
- 	headers = [("Content-Type", "text/plain")]
- 	start_response(status, headers)
- 	body = "\n".join(environ["QUERY_STRING"].split("&"))
- 	return [ body ]
- if __name__ == '__main__':
- 	pass
+ def app (environ, start_response):
+  status = '200 OK'
+  response_headers = [('Content-type','text/plain')]
+  start_response(status, response_headers)
+  resp = environ['QUERY_STRING'].split("&")
+  resp = [item+"\r\n" for item in resp]
+  return resp
