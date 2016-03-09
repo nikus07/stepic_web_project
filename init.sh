52c740dfe9cc0a01fd18184b68fe97e48ef7d61d
Muntello <muntello@gmail.com>
2016-03-09 13:45:56 +0300

    Revert "fix 3"
    This reverts commit 89ab610807812bc867eb7a43a7f7227e245c3d09.

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
+sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx.conf
 sudo /etc/init.d/nginx restart
