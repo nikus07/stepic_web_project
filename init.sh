8e6e4f5aec5aed3029653807f34eef6917e678a2
Muntello <muntello@gmail.com>
2016-03-09 22:00:22 +0300

    hello gunicorn

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo gunicorn -b 0.0.0.0:8080 hello:application
+sudo ln -sf /home/box/etc/hello.py  /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
